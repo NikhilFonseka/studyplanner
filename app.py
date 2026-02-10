6162d80d053a3b5b8f91c64dfca2f984d03348b8
Nikhil <161525763+NikhilFonseka@users.noreply.github.com>
2026-02-10 18:30:00 +1300

    login system backend

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, flash, session
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
@@ -7,3 +8,22 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "W_notes+"
 db = SQLAlchemy(app)
 
+class User(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        user = request.form.get('username')
+        pwd = request.form.get('password')
+        
+        # Turn "password123" into a secure scrambled string
+        hashed_pwd = generate_password_hash(pwd)
+        
+        new_user = User(username=user, password_hash=hashed_pwd)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('signin'))
+    return render_template('signup.html')
