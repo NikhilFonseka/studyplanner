daed40a48d8ffc62818c63bb99db072c418f10f0
Nikhil <161525763+NikhilFonseka@users.noreply.github.com>
2026-02-11 17:54:17 +1300

    comments + email sign in method  described in html

--- app.py
+++ app.py
@@ -8,13 +8,13 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "W_notes+"
 db = SQLAlchemy(app)
-
+#user table
 class User(db.Model):
     user_id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(50), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
-
+#controller for sign up page
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
@@ -29,7 +29,7 @@ def signup():
         db.session.commit()
         return redirect(url_for('signin'))
     return render_template('signup.html')
-
+#controller for sign in page
 @app.route('/signin', methods=['GET', 'POST'])
 def signin():
     if request.method == 'POST':
--- tester.py
+++ tester.py
@@ -2,7 +2,7 @@ from app import app, db, User
 
 def run_test():
     client = app.test_client()
-    
+    #erase database
     with app.app_context():
         db.drop_all()
         db.create_all()
@@ -13,7 +13,7 @@ def run_test():
             'password': '123'
         }, follow_redirects=True)
         
-
+        #query is similar to select * from user checks if the user is in the database if not it fails
         user = User.query.filter_by(username='testuser').first()
         if user:
             print("in database.")
@@ -24,10 +24,11 @@ def run_test():
             'username or email': 'test',
             'password': '123'
         }, follow_redirects=True)
-
+        #tries to sign in using the data from the created user earlier if it works pass else it fails
         if b"Login Complete" in response.data:
             print("pass")
         else:
             print("fail")
-
-run_test()
+#good practice to include name = main for importing
+if __name__ == '__main__':
+    run_test()
